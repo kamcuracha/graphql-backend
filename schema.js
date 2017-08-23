ef25d50353703419e72836dabbeb3cf9b05a1052
Kam Curacha <kamcuracha@yahoo.com>
2017-08-23 22:45:00 +0800

    Fetch json using axios

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -6,13 +7,6 @@ const {
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
-
-const customers = [
-    {id: '1', name: 'John Doe', email: 'johndoe@yahoo.com', age: 15},
-    {id: '2', name: 'Jane Doe', email: 'janedoe@yahoo.com', age: 18},
-    {id: '3', name: 'Bob Dylan', email: 'bobbie@yahoo.com', age: 40},
-    {id: '4', name: 'Joe Romano', email: 'jromano@yahoo.com', age: 39},
-];
 
 const CustomerType = new GraphQLObjectType({
     name: 'Customer',
@@ -33,11 +27,15 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args) {
-                for(let i = 0; i < customers.length; i++) {
-                    if(customers[i].id == args.id) {
-                        return customers[i];
-                    }
-                }
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                        .then(res => res.data);
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return axios.get('http://localhost:3000/customers/')
+                        .then(res => res.data);
             }
         }
     }
